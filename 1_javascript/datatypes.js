bb04c60b84d1758af1ca807ba5f69df6da662107
Abul123456789 <149576470+Abul123456789@users.noreply.github.com>
2025-10-23 10:26:31 +0000

    datatypes

---
+++ 1_javascript/datatypes.js
@@ -0,0 +1,6 @@
+"use strict" // treat all js code as newer version
+alert (3 + 3) // we are using node.js not browser
+
+console.log(typeof "30");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ 1_javascript/varaiables.js
@@ -0,0 +1,10 @@
+const account_ID=223838
+let account_email="abc@gmail.com"
+var account_password="23455"
+accountcity= "Multan"
+//account_ID=2// not allowed
+account_email="hc@gmail.com"
+account_password="3888t"
+accountcity="lahore"
+console.log(account_ID);
+console.log([account_email, account_password,accountcity]);
